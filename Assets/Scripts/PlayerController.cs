0e5eec52162495087bad6d1ee017146f8ce20b91
RankeTheDev <christianac20@educastur.es>
2026-01-21 21:26:06 +0100

    Movimiento Basico Añadido
    PlayerController se actualizo con la parte de codigo necesaria para el movimiento básico

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,43 +5,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float horizontalInput;
-    public float verticalInput;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float verticalInput;
     public float xSpeed;
     public float ySpeed;
+    public SpriteRenderer spriteRenderer;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) //Detecta cuando pulsas 
+        #region MOVEMENT
+
+        horizontalInput = Input.GetAxisRaw("Horizontal"); //Detecta cuando pulsas las flechas Izda / Dcha
+        verticalInput = Input.GetAxisRaw("Vertical"); //Detecta cuando pulsas las flechas Arriba / Abajo
+
+        transform.Translate(Vector2.right * Time.deltaTime * xSpeed * horizontalInput); //Mueve al player horizontalmente segun el horizontalInput y a la velocidad de xSpeed
+        transform.Translate(Vector2.up * Time.deltaTime * ySpeed * verticalInput); //Mueve al player horizontalmente segun el verticalInput y a la velocidad de ySpeed
+
+
+        CharacterFlip();     
+
+        #endregion MOVEMENT
+    }
+
+
+    private void CheckInputHorizontal()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void CharacterFlip()
+    {
+        if (horizontalInput < 0)
         {
-            transform.Translate(Vector2.right * Time.deltaTime * xSpeed);
+            spriteRenderer.flipX = true;
         }
-
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        else if (horizontalInput > 0.01)
         {
-            transform.Translate(Vector2.left * Time.deltaTime * xSpeed);
+            spriteRenderer.flipX = false;
         }
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector2.up * Time.deltaTime * ySpeed);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        { 
-            transform.Translate(Vector2.down * Time.deltaTime * ySpeed);
-        }
-
-
-        /*
-        horizontalInput = GetKeyDown(KeyCode.RightArrow); //Detecta cuando pulsas las flechas Izquierda / Derecha
-
-        transform.Translate(Vector2.right * Time.deltaTime * xSpeed* horizontalInput);
-
-        verticalInput = Input.GetAxis("Vertical"); //Detecta cuando pulsas las flechas Arriba / Abajo
-
-        transform.Translate(Vector2.up * Time.deltaTime * xSpeed * verticalInput);
-        */
     }
 }
