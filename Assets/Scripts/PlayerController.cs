31a799425fcc4dfc97907969e9f456bcc277551e
Christianac20 <christianac20@educastur.es>
2026-01-22 12:40:54 +0100

    Modificado el sprint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,9 @@ public class PlayerController : MonoBehaviour
     float verticalInput; //almacena el valor del input vertical (W y S, Flecha arriba y Flecha abajo)
 
     // VELOCIDADES
-    public float speedMultiplier = 1.5f; //Valor por el que multiplicar la velocidad base del player al correr
-    public float xSpeedBase = 5; //Velocidad horizontal base del player. Constante
-    [SerializeField] float xSpeed; //Velocidad horizontal base del player. Variable
-    public float ySpeedBase = 3; //Velocidad verticalbase del player. Constante
-    [SerializeField] float ySpeed; //Velocidad vertical base del player. Variable
+    public float speedMultiplier = 1; //Valor por el que multiplicar la velocidad base del player al correr
+    public float xSpeed = 5; //Velocidad horizontal base del player.
+    public float ySpeed = 3; //Velocidad vertical base del player.
     
     // USO DE COMPONENTES
     public SpriteRenderer spriteRenderer; //almacena el componente SpriteRenderer del player
@@ -24,8 +22,6 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>(); //Coge el spriteRenderer del player
-        xSpeed = xSpeedBase; //Asigna la velocidad x 
-        ySpeed = ySpeedBase; //Asigna la velocidad y
     }
 
     void Update()
@@ -33,16 +29,13 @@ public class PlayerController : MonoBehaviour
         #region MOVEMENT
 
         // SPRINT
-
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            xSpeed = xSpeedBase * speedMultiplier; //multiplica la velocidad en X al correr
-            ySpeed = ySpeedBase * speedMultiplier; //multiplica la velocidad en Y al correr
+            speedMultiplier = 1.5f;
         }
         else
         {
-            ySpeed = ySpeedBase; //resetea la velocidad en X a su valor Base
-            xSpeed = xSpeedBase; //resetea la velocidad en Y a su valor Base
+            speedMultiplier = 1;
         }
 
         // MOVIMIENTO BASICO
@@ -50,9 +43,8 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal"); //Detecta cuando pulsas las flechas Izda / Dcha
         verticalInput = Input.GetAxisRaw("Vertical"); //Detecta cuando pulsas las flechas Arriba / Abajo
 
-        transform.Translate(Vector2.right * Time.deltaTime * xSpeed * horizontalInput); //Mueve al player horizontalmente segun el horizontalInput y a la velocidad de xSpeed
-        transform.Translate(Vector2.up * Time.deltaTime * ySpeed * verticalInput); //Mueve al player horizontalmente segun el verticalInput y a la velocidad de ySpeed
-
+        transform.Translate(Vector2.right * Time.deltaTime * xSpeed * speedMultiplier * horizontalInput); //Mueve al player horizontalmente segun el horizontalInput y a la velocidad de xSpeed, ademas de multiplicarlo por el valor de speedmultiplier para que corra
+        transform.Translate(Vector2.up * Time.deltaTime * ySpeed * speedMultiplier * verticalInput); //Mueve al player horizontalmente segun el verticalInput y a la velocidad de ySpeed, ademas de multiplicarlo por el valor de speedmultiplier para que corra
 
         CharacterFlip(); //Voltea al player para que mire hacia la direccion en que se mueve     
 
